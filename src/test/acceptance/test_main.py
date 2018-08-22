0afeea0cb75c67f793948cb41f004fd3255038df
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-08-22 08:38:08 +0200

    2: requested changes

--- src/test/acceptance/test_main.py
+++ src/test/acceptance/test_main.py
@@ -11,8 +11,8 @@ MAINSCRIPT = Path(SRC_DIR, 'fact_search_and_download.py')
     ('-V', 'FACT Search and Download', 0),
     ('-q {{', 'Invalid json:', 1),
     ('', 'No query given. Use -q or -Q option', 1),
-    ('-H http://not_existing -q {}', 'Host not reachable', 1),
-    ('-H http://not_existing -q {} -F', 'Host not reachable', 1)
+    ('-H http://not_existing -q {}', 'FACT not reachable', 1),
+    ('-H http://not_existing -q {} -F', 'FACT not reachable', 1)
 ])
 def test_main_program(function, expected_in_output, expected_return_code):
         output, return_code = execute_shell_command_get_return_code('{} {}'.format(MAINSCRIPT, function), timeout=5)
